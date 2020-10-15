166e555bc5dc161c7bbbd44f63adaeb9d29e6db9
MarkRomashkov <markromashkov@mail.ru>
2020-10-15 09:51:31 +0300

    Initial Commit

---
+++ VK_Client_Romashkov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_Client_Romashkov
+//
+//  Created by Mark on 15.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
