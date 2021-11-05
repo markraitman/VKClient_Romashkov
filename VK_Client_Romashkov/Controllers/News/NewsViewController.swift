d756de42f5fd4fed58cb463910d0b9b5cadd42c9
MarkRomashkov <markromashkov@mail.ru>
2021-11-05 21:17:32 +0300

    Corrected UI of TextView and ImageView

--- VK_Client_Romashkov/Controllers/News/NewsViewController.swift
+++ VK_Client_Romashkov/Controllers/News/NewsViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 enum PostCellType: Int, CaseIterable {
     case author
-    case text
-    case photo
+    case content
     case likeCount
 }
 
@@ -28,23 +27,28 @@ final class NewsViewController: UITableViewController {
         News(newsAuthorAvatar: UIImage(named: "Habr"),
              newsAuthorName: "Habr",
              newsDate: "08.01.2021",
-             newsText: "Электромеханический арифмометр ВК-2. Название расшифровывается как Вычислитель Клавишный, вторая модель. Слово «клавишный», вынесенное в название, призвано подчеркнуть одно из основных достоинств машины — ввод чисел и операций с помощью клавиш: http://amp.gs/MIyZ"),
+             newsText: "Электромеханический арифмометр ВК-2. Название расшифровывается как Вычислитель Клавишный, вторая модель. Слово «клавишный», вынесенное в название, призвано подчеркнуть одно из основных достоинств машины — ввод чисел и операций с помощью клавиш: http://amp.gs/MIyZ",
+             type: .post),
         News(newsAuthorAvatar: UIImage(named: "ITHumor"),
              newsAuthorName: "ITHumor",
              newsDate: "07.01.2021",
-             newsPhoto: UIImage(named: "followers")),
+             newsPhoto: UIImage(named: "followers"),
+             type: .image),
         News(newsAuthorAvatar: UIImage(named: "GeekBrains"),
              newsAuthorName: "GeekBrains",
              newsDate: "07.01.2021",
-             newsText: "Как попасть в геймдев? Именно этой теме был посвящен осенний митап, который мы провели совместно с TalentsInGames. На встрече обсудили поиск работы после курсов, собеседования, зарплату на стартовых позициях и многое другое. Всю информацию отразили для вас в статье: http://amp.gs/MIN3"),
+             newsText: "Как попасть в геймдев? Именно этой теме был посвящен осенний митап, который мы провели совместно с TalentsInGames. На встрече обсудили поиск работы после курсов, собеседования, зарплату на стартовых позициях и многое другое. Всю информацию отразили для вас в статье: http://amp.gs/MIN3",
+             type: .post),
         News(newsAuthorAvatar: UIImage(named: "IamProgrammist"),
              newsAuthorName: "IamProgrammist",
              newsDate: "06.01.2021",
-             newsPhoto: UIImage(named: "newCode")),
+             newsPhoto: UIImage(named: "newCode"),
+             type: .image),
         News(newsAuthorAvatar: UIImage(named: "ITHumor"),
              newsAuthorName: "ITHumor",
              newsDate: "05.01.2021",
-             newsPhoto: UIImage(named: "goodNight")),
+             newsPhoto: UIImage(named: "goodNight"),
+             type: .image),
     ]
     
     // MARK: - Section
@@ -73,22 +77,23 @@ final class NewsViewController: UITableViewController {
             cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
             return cell
             
-        case .text:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "TextOfFeedTableViewCell", for: indexPath) as! TextOfFeedTableViewCell
-            cell.newsTextView.text = item.newsText
-            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
-            return cell
-            
-        case .photo:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoOfFeedTableViewCell", for: indexPath) as! PhotoOfFeedTableViewCell
-            cell.newsPhoto.image = item.newsPhoto
-            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
-            return cell
-            
+        case .content:
+            switch item.type {
+            case .post:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "TextOfFeedTableViewCell", for: indexPath) as! TextOfFeedTableViewCell
+                cell.newsTextView.text = item.newsText
+                cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+                return cell
+            case .image:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoOfFeedTableViewCell", for: indexPath) as! PhotoOfFeedTableViewCell
+                cell.newsPhoto.image = item.newsPhoto
+                cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+                return cell
+            }
+ 
         case .likeCount:
             let cell = tableView.dequeueReusableCell(withIdentifier: "LikeCountTableViewCell", for: indexPath) as! LikeCountTableViewCell
             return cell
-            
         default:
             return UITableViewCell()
         }
